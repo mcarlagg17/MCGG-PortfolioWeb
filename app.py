dff4c2942144bcdf55f63247090ef066380e4161
mcarlagg17 <carla.glezz@gmail.com>
2023-03-14 17:30:40 +0100

    version 1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+import jinja2
+
+app = Flask(__name__,static_folder='static')
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+            
