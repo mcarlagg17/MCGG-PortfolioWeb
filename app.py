8cb6015a6500df51054aec9f592d16124eee8424
mcarlagg17 <carla.glezz@gmail.com>
2023-03-15 12:38:04 +0100

    app.py modified

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
+#from flask_bootstrap import Bootstrap
 import jinja2
 
 app = Flask(__name__,static_folder='static')
-bootstrap = Bootstrap(app)
+#bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home():
